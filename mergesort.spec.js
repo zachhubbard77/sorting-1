9dc87135997ec7e2bf834c8df78b50ca14c3dfe5
SkwibHub <eidolongroups@gmail.com>
2018-11-05 13:19:09 -0800

    "Added Merge sort specs"

---
+++ mergesort.spec.js
@@ -0,0 +1,13 @@
+describe('Split Array function', function() {
+    var array;
+    beforeEach(function () {
+        sortedArray = [1, 4, 12, 15, 18, 34, 56, 77, 78, 80];
+        unsortedArray = [23, 5, 86, 32, 11, 45, 72, 15, 32];
+        stringArray = ['apple', 'dog', 'cat', 'banana']
+    })
+    
+    it('is able to split an array into two halves', function() {
+      // your code here 
+    });
+
+  });
